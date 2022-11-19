945e57d159c95b28131c13d46c972a83c28d888d
Rohan Singh <72556733+rohandocx@users.noreply.github.com>
2022-11-19 18:36:25 +0530

    Create 35. Search into insert position.cpp

---
+++ 35. Search into insert position.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    public: int searchInsert(int[] nums, int target) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] < target)count++;
+        }
+        return count;
+    }
+}
